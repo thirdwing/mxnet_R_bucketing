e6812c15273c07d4da2a223806c54125af682ddf
Qiang Kou <qkou@qkou.info>
2017-07-26 17:31:17 -0700

    Test file

---
+++ test_lstm.R
@@ -0,0 +1,91 @@
+require("mxnet")
+
+source("mx_io_bucket_iter.R")
+source("rnn_bucket_setup.R")
+source("rnn_bucket_train.R")
+
+corpus_bucketed_train <-
+  readRDS(file = "corpus_bucketed_train_100_200_300_500_800_left.rds")
+corpus_bucketed_test <-
+  readRDS(file = "corpus_bucketed_test_100_200_300_500_800_left.rds")
+
+vocab <- length(corpus_bucketed_test$dic)
+
+### Create iterators
+batch_size = 64
+
+X_iter_train <- mx_io_bucket_iter(buckets = corpus_bucketed_train$buckets,
+                                  batch_size = batch_size,
+                                  data_mask_element = 0,
+                                  shuffle = TRUE)
+
+X_iter_test <- mx_io_bucket_iter(buckets = corpus_bucketed_test$buckets,
+                                 batch_size = batch_size,
+                                 data_mask_element = 0,
+                                 shuffle = FALSE)
+
+num.label = 2
+num.embed = 16
+num.hidden = 24
+update.period = 1
+
+metric <- mx.metric.accuracy
+
+input.size = vocab
+initializer = mx.init.Xavier(rnd_type = "gaussian",
+                             factor_type = "in",
+                             magnitude = 2)
+dropout = 0.25
+verbose = TRUE
+
+batch.end.callback <- mx.callback.log.train.metric(period = 50)
+epoch.end.callback <- mx.callback.log.train.metric(period = 1)
+
+seq.len <- 2
+
+rnn_graph <- rnn.unroll(seq.len = seq.len,
+                        num.rnn.layer = 1,
+                        num.hidden = num.hidden,
+                        input.size = input.size,
+                        num.embed = num.embed,
+                        num.label = num.label,
+                        dropout = dropout,
+                        ignore_label = 0,
+                        config = "seq-to-one")
+
+graph.viz(rnn_graph, type = "graph", direction = "TD")
+
+devices <- list(mx.cpu())
+end.round = 16
+
+optimizer <- mx.opt.create("adadelta",
+                           rho = 0.92,
+                           epsilon = 1e-6,
+                           wd = 0.0002,
+                           clip_gradient = NULL,
+                           rescale.grad = 1 / batch_size)
+
+num.rnn.layer <- 2
+
+model_sentiment_lstm <- mx.rnn.buckets(train.data =  X_iter_train,
+                                       eval.data = X_iter_test,
+                                       begin.round = 1,
+                                       end.round = end.round,
+                                       ctx = devices,
+                                       metric = metric,
+                                       optimizer = optimizer,
+                                       kvstore = "local",
+                                       num.rnn.layer = num.rnn.layer,
+                                       num.embed = num.embed,
+                                       num.hidden = num.hidden,
+                                       num.label = num.label,
+                                       input.size = input.size,
+                                       update.period = 1,
+                                       initializer = initializer,
+                                       dropout = dropout,
+                                       config = "seq-to-one",
+                                       batch.end.callback = batch.end.callback,
+                                       epoch.end.callback = epoch.end.callback,
+                                       verbose = TRUE)
+
+mx.model.save(model_sentiment_lstm, prefix = "model_sentiment_lstm", iteration = end.round)
