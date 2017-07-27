2320a7cf38d045ab03794ed15fba096576a257af
Qiang Kou <qkou@qkou.info>
2017-07-27 11:05:07 -0700

    bucket iter clean up

--- rnn_bucket_train.R
+++ rnn_bucket_train.R
@@ -1,58 +1,65 @@
 # Internal function to do multiple device training on RNN
 mx.model.train.rnn.buckets <- function(ctx,
                                        sym_list,
-                                       arg.params, 
+                                       arg.params,
                                        aux.params,
                                        input.shape,
-                                       begin.round, 
-                                       end.round, 
+                                       begin.round,
+                                       end.round,
                                        optimizer,
-                                       train.data, 
+                                       train.data,
                                        eval.data,
                                        metric,
                                        epoch.end.callback,
                                        batch.end.callback,
                                        kvstore,
-                                       verbose=TRUE,
-                                       batch_size) {
-  
+                                       verbose = TRUE) {
   ndevice <- length(ctx)
-  if(verbose) cat(paste0("Start training with ", ndevice, " devices\n"))
+  if (verbose)
+    message(paste0("Start training with ", ndevice, " devices"))
   
   ###################################
   ## Initialisation
   # Get the input names
-  symbol<- sym_list[[names(train.data$bucketID())]]
+  symbol <- sym_list[[names(train.data$bucketID)]]
   
   input.names <- names(input.shape)
-  arg.names<- names(arg.params)
+  arg.names <- names(arg.params)
   
   # Grad request
-  grad_req<- rep("write", length(symbol$arguments))
-  grad_null_idx<- match(input.names, symbol$arguments)
-  grad_req[grad_null_idx]<- "null"
+  grad_req <- rep("write", length(symbol$arguments))
+  grad_null_idx <- match(input.names, symbol$arguments)
+  grad_req[grad_null_idx] <- "null"
   
   # Arg array order
-  update_names<- c(input.names, arg.names)
-  arg_update_idx<- match(symbol$arguments, update_names)
+  update_names <- c(input.names, arg.names)
+  arg_update_idx <- match(symbol$arguments, update_names)
   
   # Initial input shapes - need to be adapted for multi-devices - divide highest dimension by device nb
-  s<- sapply(input.shape, function(shape){
-    mx.nd.zeros(shape=shape, ctx = mx.cpu())
+  s <- sapply(input.shape, function(shape) {
+    mx.nd.zeros(shape = shape, ctx = mx.cpu())
   })
   
   #####################################################
   ### Initial binding
   train.execs <- lapply(1:ndevice, function(i) {
-    mxnet:::mx.symbol.bind(symbol = symbol, arg.arrays = c(s, arg.params)[arg_update_idx], aux.arrays = aux.params, ctx=ctx[[i]], grad.req=grad_req)
+    mxnet:::mx.symbol.bind(
+      symbol = symbol,
+      arg.arrays = c(s, arg.params)[arg_update_idx],
+      aux.arrays = aux.params,
+      ctx = ctx[[i]],
+      grad.req = grad_req
+    )
   })
-
+  
   # KVStore related stuffs
   params.index <-
-    as.integer(mx.util.filter.null(
-      lapply(1:length(train.execs[[1]]$ref.grad.arrays), function(k) {
-        if (!is.null(train.execs[[1]]$ref.grad.arrays[[k]])) k else NULL
-      })))
+    as.integer(mx.util.filter.null(lapply(1:length(train.execs[[1]]$ref.grad.arrays), function(k) {
+      if (!is.null(train.execs[[1]]$ref.grad.arrays[[k]]))
+        k
+      else
+        NULL
+    })))
   update.on.kvstore <- FALSE
   if (!is.null(kvstore) && kvstore$update.on.kvstore) {
     update.on.kvstore <- TRUE
@@ -75,29 +82,42 @@ mx.model.train.rnn.buckets <- function(ctx,
     train.data$reset()
     
     while (train.data$iter.next()) {
-      
-      seq_len<- as.integer(names(train.data$bucketID()))
+      # seq_len<- as.integer(names(train.data$bucketID()))
       
       # Get inputs from iterator
       dlist <- train.data$value()
       
       # Slice inputs for multi-devices
       slices <- lapply(dlist[input.names], function(input) {
-        mx.nd.SliceChannel(data=input, num_outputs = ndevice, axis = 0, squeeze_axis = F)
+        mx.nd.SliceChannel(
+          data = input,
+          num_outputs = ndevice,
+          axis = 0,
+          squeeze_axis = F
+        )
       })
       
       ### Get the new symbol
       ### Bind the arguments from previous executor state and symbol for the BucketID
-      symbol<- sym_list[[names(train.data$bucketID())]]
+      symbol <- sym_list[[names(train.data$bucketID)]]
       
       train.execs <- lapply(1:ndevice, function(i) {
-        if (ndevice>1) s <- lapply(slices, function(x) x[[i]]) else 
-          s<- slices
-        mxnet:::mx.symbol.bind(symbol = symbol, arg.arrays = c(s, train.execs[[i]]$arg.arrays[arg.names])[arg_update_idx], aux.arrays = train.execs[[i]]$aux.arrays, ctx=ctx[[i]], grad.req=grad_req)
+        if (ndevice > 1)
+          s <- lapply(slices, function(x)
+            x[[i]])
+        else
+          s <- slices
+        mxnet:::mx.symbol.bind(
+          symbol = symbol,
+          arg.arrays = c(s, train.execs[[i]]$arg.arrays[arg.names])[arg_update_idx],
+          aux.arrays = train.execs[[i]]$aux.arrays,
+          ctx = ctx[[i]],
+          grad.req = grad_req
+        )
       })
       
       for (texec in train.execs) {
-        mx.exec.forward(texec, is.train=TRUE)
+        mx.exec.forward(texec, is.train = TRUE)
       }
       
       # copy outputs to CPU
@@ -115,40 +135,54 @@ mx.model.train.rnn.buckets <- function(ctx,
       }
       if (!is.null(kvstore)) {
         # push the gradient
-        kvstore$push(params.index, lapply(train.execs, function(texec) {
-          texec$ref.grad.arrays[params.index]
-        }), -params.index)
+        kvstore$push(params.index,
+                     lapply(train.execs, function(texec) {
+                       texec$ref.grad.arrays[params.index]
+                     }),
+                     -params.index)
       }
       if (update.on.kvstore) {
         # pull back weight
-        kvstore$pull(params.index, lapply(train.execs, function(texec) {
-          texec$ref.arg.arrays[params.index]
-        }), -params.index)
+        kvstore$pull(params.index,
+                     lapply(train.execs, function(texec) {
+                       texec$ref.arg.arrays[params.index]
+                     }),
+                     -params.index)
       } else {
         # pull back gradient sums
         if (!is.null(kvstore)) {
-          kvstore$pull(params.index, lapply(train.execs, function(texec) {
-            texec$ref.grad.arrays[params.index]
-          }), -params.index)
+          kvstore$pull(params.index,
+                       lapply(train.execs, function(texec) {
+                         texec$ref.grad.arrays[params.index]
+                       }),
+                       -params.index)
         }
         arg.blocks <- lapply(1:ndevice, function(i) {
-          updaters[[i]](train.execs[[i]]$ref.arg.arrays, train.execs[[i]]$ref.grad.arrays)
+          updaters[[i]](train.execs[[i]]$ref.arg.arrays,
+                        train.execs[[i]]$ref.grad.arrays)
         })
         for (i in 1:ndevice) {
-          mx.exec.update.arg.arrays(train.execs[[i]], arg.blocks[[i]], skip.null=TRUE)
+          mx.exec.update.arg.arrays(train.execs[[i]], arg.blocks[[i]], skip.null =
+                                      TRUE)
         }
       }
       # Update the evaluation metrics
       if (!is.null(metric)) {
-        if (ndevice==1) {
-          train.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric)
-          #train.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric, seq_len=seq_len, batch_size=batch_size)
-          #train.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric)
+        if (ndevice == 1) {
+          train.metric <-
+            metric$update(
+              label = mx.nd.Reshape(slices$label, shape = -1),
+              pred = out.preds[[i]][[length(out.preds[[i]])]],
+              state = train.metric
+            )
         } else{
           for (i in 1:ndevice) {
-            train.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric)
-            #train.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric, seq_len=seq_len, batch_size=batch_size)
-            #train.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = train.metric)
+            train.metric <-
+              metric$update(
+                label = mx.nd.Reshape(slices[[i]]$label, shape = -1),
+                pred = out.preds[[i]][[length(out.preds[[i]])]],
+                state = train.metric
+              )
           }
         }
       }
@@ -162,7 +196,8 @@ mx.model.train.rnn.buckets <- function(ctx,
     
     if (!is.null(metric)) {
       result <- metric$get(train.metric)
-      if(verbose) cat(paste0("[", iteration, "] Train-", result$name, "=", result$value, "\n"))
+      if (verbose)
+        message(paste0("[", iteration, "] Train-", result$name, "=", result$value))
     }
     
     if (!is.null(eval.data)) {
@@ -171,25 +206,38 @@ mx.model.train.rnn.buckets <- function(ctx,
       }
       eval.data$reset()
       while (eval.data$iter.next()) {
-        
-        seq_len<- as.integer(names(eval.data$bucketID()))
+        seq_len <- as.integer(names(eval.data$bucketID()))
         
         # Get input data slice
         dlist <- eval.data$value()
         slices <- lapply(dlist[input.names], function(input) {
-          mx.nd.SliceChannel(data=input, num_outputs = ndevice, axis = 0, squeeze_axis = F)
+          mx.nd.SliceChannel(
+            data = input,
+            num_outputs = ndevice,
+            axis = 0,
+            squeeze_axis = F
+          )
         })
         
         symbol = sym_list[[names(eval.data$bucketID())]]
-
+        
         train.execs <- lapply(1:ndevice, function(i) {
-          if (ndevice>1) s <- lapply(slices, function(x) x[[i]]) else 
-            s<- slices
-          mxnet:::mx.symbol.bind(symbol = symbol, arg.arrays = c(s, train.execs[[i]]$arg.arrays[arg.names])[arg_update_idx], aux.arrays = train.execs[[i]]$aux.arrays, ctx=ctx[[i]], grad.req=grad_req)
+          if (ndevice > 1)
+            s <- lapply(slices, function(x)
+              x[[i]])
+          else
+            s <- slices
+          mxnet:::mx.symbol.bind(
+            symbol = symbol,
+            arg.arrays = c(s, train.execs[[i]]$arg.arrays[arg.names])[arg_update_idx],
+            aux.arrays = train.execs[[i]]$aux.arrays,
+            ctx = ctx[[i]],
+            grad.req = grad_req
+          )
         })
         
         for (texec in train.execs) {
-          mx.exec.forward(texec, is.train=FALSE)
+          mx.exec.forward(texec, is.train = FALSE)
         }
         
         # copy outputs to CPU
@@ -202,15 +250,21 @@ mx.model.train.rnn.buckets <- function(ctx,
         })
         
         if (!is.null(metric)) {
-          if (ndevice==1) {
-            eval.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric)
-            #eval.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric, seq_len=seq_len, batch_size=batch_size)
-            #eval.metric <- metric$update(label = mx.nd.Reshape(slices$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric)
+          if (ndevice == 1) {
+            eval.metric <-
+              metric$update(
+                label = mx.nd.Reshape(slices$label, shape = -1),
+                pred = out.preds[[i]][[length(out.preds[[i]])]],
+                state = eval.metric
+              )
           } else{
             for (i in 1:ndevice) {
-              eval.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric)
-              #eval.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric, seq_len=seq_len, batch_size=batch_size)
-              #eval.metric <- metric$update(label = mx.nd.Reshape(slices[[i]]$label, shape=-1), pred = out.preds[[i]][[length(out.preds[[i]])]], state = eval.metric)
+              eval.metric <-
+                metric$update(
+                  label = mx.nd.Reshape(slices[[i]]$label, shape = -1),
+                  pred = out.preds[[i]][[length(out.preds[[i]])]],
+                  state = eval.metric
+                )
             }
           }
         }
@@ -218,7 +272,15 @@ mx.model.train.rnn.buckets <- function(ctx,
       #eval.data$reset()
       if (!is.null(metric)) {
         result <- metric$get(eval.metric)
-        if(verbose) cat(paste0("[", iteration, "] Validation-", result$name, "=", result$value, "\n"))
+        if (verbose)
+          message(paste0(
+            "[",
+            iteration,
+            "] Validation-",
+            result$name,
+            "=",
+            result$value
+          ))
       }
     } else {
       eval.metric <- NULL
@@ -228,7 +290,8 @@ mx.model.train.rnn.buckets <- function(ctx,
     
     epoch_continue <- TRUE
     if (!is.null(epoch.end.callback)) {
-      epoch_continue <- epoch.end.callback(iteration, 0, environment(), verbose = verbose)
+      epoch_continue <-
+        epoch.end.callback(iteration, 0, environment(), verbose = verbose)
     }
     
     if (!epoch_continue) {
@@ -237,4 +300,3 @@ mx.model.train.rnn.buckets <- function(ctx,
   }
   return(model)
 }
-
--- test_lstm.R
+++ test_lstm.R
@@ -14,19 +14,13 @@ vocab <- length(corpus_bucketed_test$dic)
 ### Create iterators
 batch_size = 64
 
-train.data <- mx_io_bucket_iter(
-  buckets = corpus_bucketed_train$buckets,
-  batch_size = batch_size,
-  data_mask_element = 0,
-  shuffle = TRUE
-)
-
-eval.data <- mx_io_bucket_iter(
-  buckets = corpus_bucketed_test$buckets,
-  batch_size = batch_size,
-  data_mask_element = 0,
-  shuffle = FALSE
-)
+train.data <-
+  mx.io.bucket.iter(
+    buckets = corpus_bucketed_train$buckets,
+    batch.size = batch_size,
+    data.mask.element = 0,
+    shuffle = TRUE
+  )
 
 num.label = 2
 num.embed = 16
@@ -43,36 +37,46 @@ dropout = 0.25
 verbose = TRUE
 
 devices <- list(mx.cpu())
-end.round = 16
+end.round = 3
 
-optimizer <- mx.opt.create("adadelta",
-                           rho = 0.92,
-                           epsilon = 1e-6,
-                           wd = 0.0002,
-                           clip_gradient = NULL,
-                           rescale.grad = 1 / batch_size)
+optimizer <- mx.opt.create(
+  "adadelta",
+  rho = 0.92,
+  epsilon = 1e-6,
+  wd = 0.0002,
+  clip_gradient = NULL,
+  rescale.grad = 1 / batch_size
+)
 
 num.rnn.layer <- 2
 
-model_sentiment_lstm <- mx.rnn.buckets(train.data =  train.data,
-                                       eval.data = eval.data,
-                                       begin.round = 1,
-                                       end.round = end.round,
-                                       ctx = devices,
-                                       metric = metric,
-                                       optimizer = optimizer,
-                                       kvstore = "local",
-                                       num.rnn.layer = num.rnn.layer,
-                                       num.embed = num.embed,
-                                       num.hidden = num.hidden,
-                                       num.label = num.label,
-                                       input.size = input.size,
-                                       update.period = 1,
-                                       initializer = initializer,
-                                       dropout = dropout,
-                                       config = "seq-to-one",
-                                       batch.end.callback = mx.callback.log.train.metric(period = 50),
-                                       epoch.end.callback = NULL,
-                                       verbose = TRUE)
+cell.type = "lstm"
 
-mx.model.save(model_sentiment_lstm, prefix = "model_sentiment_lstm", iteration = end.round)
+config = "seq-to-one"
+
+model_sentiment_lstm <- mx.rnn.buckets(
+  train.data =  train.data,
+#  eval.data = eval.data,
+  begin.round = 1,
+  end.round = end.round,
+  ctx = devices,
+  metric = metric,
+  optimizer = optimizer,
+  kvstore = "local",
+  num.rnn.layer = num.rnn.layer,
+  num.embed = num.embed,
+  num.hidden = num.hidden,
+  num.label = num.label,
+  input.size = input.size,
+  update.period = 1,
+  initializer = initializer,
+  dropout = dropout,
+  config = "seq-to-one",
+  batch.end.callback = mx.callback.log.train.metric(period = 50),
+  epoch.end.callback = NULL,
+  verbose = TRUE
+)
+
+mx.model.save(model_sentiment_lstm,
+              prefix = "model_sentiment_lstm",
+              iteration = end.round)
